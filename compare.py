2dec1d52114683e12ef50c05f40dd87bc359b64a
rehan <rr1537460@gmail.com>
2025-07-04 14:03:04 +0530

    comparision type

---
+++ compare.py
@@ -0,0 +1,8 @@
+iphone_15=70000
+iphone_16=82000
+print(iphone_15<iphone_16)
+print(iphone_15>iphone_16)
+print(iphone_15<=iphone_16)
+print(iphone_15>=iphone_16)
+print(iphone_15==iphone_16)
+print(iphone_15!=iphone_16)
